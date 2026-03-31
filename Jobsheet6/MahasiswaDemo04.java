f1acf23906a6674fdef2413c9c8002e24f179273
NamaKamu <emailkamu@gmail.com>
2026-03-31 10:22:38 +0700

    modifikasi

--- Jobsheet6/MahasiswaDemo04.java
+++ Jobsheet6/MahasiswaDemo04.java
@@ -1,25 +1,35 @@
 package Jobsheet6;
 
+import java.util.Scanner;
+
 public class MahasiswaDemo04 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         MahasiswaBerprestasi04 list = new MahasiswaBerprestasi04();
-        Mahasiswa04 m1 = new Mahasiswa04("123", "Zidan", "2A", 3.2);
-        Mahasiswa04 m2 = new Mahasiswa04("124", "Ayu", "2A", 3.5);
-        Mahasiswa04 m3 = new Mahasiswa04("125", "Sofi", "2A", 3.1);
-        Mahasiswa04 m4 = new Mahasiswa04("126", "Sita", "2A", 3.9);
-        Mahasiswa04 m5 = new Mahasiswa04("127", "Miki", "2A", 3.7);
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int n = sc.nextInt();
+        sc.nextLine();
 
-        list.tambah(m1);
-        list.tambah(m2);    
-        list.tambah(m3);
-        list.tambah(m4);
-        list.tambah(m5);
-
-        System.out.println("Data sebelum diurutkan:");
+        for (int i = 0; i < n; i++) {
+            System.out.println("\nData Mahasiswa ke-" + (i + 1));
+            System.out.print("NIM   : ");
+            String nim = sc.nextLine();
+            System.out.print("Nama  : ");
+            String nama = sc.nextLine();
+            System.out.print("Kelas : ");
+            String kelas = sc.nextLine();
+            System.out.print("IPK   : ");
+            double ipk = sc.nextDouble();
+            sc.nextLine();
+            Mahasiswa04 m = new Mahasiswa04(nim, nama, kelas, ipk);
+            list.tambah(m);
+        }
+        System.out.println("\n=== DATA SEBELUM SORTING ===");
         list.tampil();
 
         list.bubbleSort();
-        System.out.println("Data setelah diurutkan berdasarkan IPK (Descending):");
+        System.out.println("\n=== BUBBLE SORT (DESCENDING) ===");
         list.tampil();
+
     }
 }
