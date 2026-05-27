0edcbe9bd94f695c989e344747de0a63f37deb51
NamaKamu <emailkamu@gmail.com>
2026-05-27 14:31:28 +0700

    Pratikum 1

---
+++ Jobsheet14/BinaryTreeMain04.java
@@ -0,0 +1,38 @@
+public class BinaryTreeMain04 {
+    public static void main(String[] args) {
+        BinaryTree04 bst = new BinaryTree04();
+
+        bst.add(new Mahasiswa04("244160121", "Ali", "A", 3.57));
+        bst.add(new Mahasiswa04("244160221", "Badar", "B", 3.85));
+        bst.add(new Mahasiswa04("244160185", "Candra", "C", 3.21));
+        bst.add(new Mahasiswa04("244160220", "Dewi", "B", 3.54));
+
+        System.out.println("\nDaftar semua Mahasiswa (in order traversal):");
+        bst.traverseInOrder(bst.root);
+
+        System.out.println("\nPencarian Data mahasiswa :");
+        System.out.println("Cari Mahasiswa dengan ipk : 3.54 : ");
+        String hasilCari = bst.find(3.54) ? "Ditemukan" : "Tidak Ditemukan";
+        System.out.println(hasilCari);
+
+        System.out.println("Cari Mahasiswa dengan ipk : 3.22 : ");
+        hasilCari = bst.find(3.22) ? "Ditemukan" : "Tidak Ditemukan";
+        System.out.println(hasilCari);
+
+        bst.add(new Mahasiswa04("244160131", "Devi", "A", 3.72));
+        bst.add(new Mahasiswa04("244160205", "Ehsan", "D", 3.37));
+        bst.add(new Mahasiswa04("244160170", "Fizi", "B", 3.46));
+        System.out.println("\nDaftar semua Mahasiswa setelah penambahan 3 mahasiswa:");
+        System.out.println("InOrder Traversal:");
+        bst.traverseInOrder(bst.root);
+        System.out.println("\nPreOrder Traversal:");
+        bst.traversePreOrder(bst.root);
+        System.out.println("\nPostOrder Traversal:");
+        bst.traversePostOrder(bst.root);
+
+        System.out.println("\nPenghapusan Data mahasiswa :");
+        bst.delete(3.57);
+        System.out.println("\nDaftar semua Mahasiswa setelah penghapusan 1 mahasiswa (in order traversal):");
+        bst.traverseInOrder(bst.root);
+    }
+}
